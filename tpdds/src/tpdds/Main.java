bbb7f64cbdf9346606d13c9ee519ee121da4c6ed
andresjmichel <andres13@gmail.com>
2016-04-24 14:40:49 -0300

    arme un main de prueba ya estaria todo lo necesario para la primera entrega

--- tpdds/src/tpdds/Main.java
+++ tpdds/src/tpdds/Main.java
@@ -4,13 +4,53 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				
-        // latitud y logitud
-		Location loc1 = new Location( -34.621854, -58.402526); //"Mi casa",
-        Location loc2 = new Location( -34.635926, -58.363816); //"La Bombonera", 
-        double distance = loc1.distanceTo(loc2);
-        System.out.printf("%6.3f Kilometros desde\n", distance);
-        System.out.println(loc1 + " a " + loc2);
+		
+		//Generacion a modo de prueba de un poi, faltan armar los contructores de todos salvo localizacion.
+		
+		Direccion direccionCasa = new Direccion();
+		
+		direccionCasa.setCallePrincipal("Av.Jujuy");
+		direccionCasa.setCalleLateralIzq("Humberto 1ro");
+		direccionCasa.setCalleLateralDer("Carlos Calvo");
+		direccionCasa.setAltura(1218);
+		
+		Location ubicacionCasa = new Location( -34.621854, -58.402526);
+
+		Poi casa = new Poi();		
+		casa.setNombre("Mi casa");		
+		//El tipo esta con numeros para que despues creemos una tabla de tipos, con una referencia numerica, eso despues lo ponemos
+		//creamos un case y listo, para que no carguemos de varias formas distintas lo mismo
+		casa.setTipo(3);
+		casa.setDireccion(direccionCasa);
+		casa.setGeoloc(ubicacionCasa);
+		
+		//Otro Poi prueba
+		
+		Direccion direccionBombo = new Direccion();
+		
+		direccionBombo.setCallePrincipal("Brandsen");
+		direccionBombo.setCalleLateralIzq("Iberlucea");
+		direccionBombo.setCalleLateralDer("Filiberto");
+		direccionBombo.setAltura(1218);
+		
+		Location ubicacionBombo = new Location( -34.635926, -58.363816);
+
+		Poi bombonera = new Poi();		
+		bombonera.setNombre("Bombonera");		
+		//El tipo esta con numeros para que despues creemos una tabla de tipos, con una referencia numerica, eso despues lo ponemos
+		//creamos un case y listo, para que no carguemos de varias formas distintas lo mismo
+		bombonera.setTipo(2);
+		bombonera.setDireccion(direccionBombo);
+		bombonera.setGeoloc(ubicacionBombo);
+		double distancia = casa.calcularDistanciaA(bombonera.getGeoloc());
+		
+		
+		System.out.println("El Poi "+ casa.getNombre() + " Ubicado en "+ casa.getDireccion() + " es una " + casa.getTipo()+ " y su geolocalizacion es " + casa.getGeoloc());
+		System.out.println("El Poi "+ bombonera.getNombre() + " Ubicado en "+ bombonera.getDireccion() + " es una " + bombonera.getTipo()+ " y su geolocalizacion es " + bombonera.getGeoloc());		
+		
+		System.out.println("La distancia entre " + casa.getNombre() + " y " + bombonera.getNombre() + " es ");
+		System.out.printf("%6.3f Kilometros \n", distancia);
+
 		
 	}
 }
--- tpdds/src/tpdds/Poi.java
+++ tpdds/src/tpdds/Poi.java
@@ -3,18 +3,20 @@ package tpdds;
 public class Poi {
 	
 	String nombre;
+	int tipo;
+	Direccion direccion;
+    Location geoloc;
+    Estadistica estadistica;
+    
+    
 	public int getTipo() {
 		return tipo;
 	}
 
 	public void setTipo(int tipo) {
+		
 		this.tipo = tipo;
 	}
-
-	int tipo;
-	Direccion direccion;
-    GeoLocalizacion geoloc;
-    Estadistica estadistica;
 	
 	public String getNombre() {
 		return nombre;
@@ -24,19 +26,19 @@ public class Poi {
 		this.nombre = nombre;
 	}
 
-	public Direccion getDireccion() {
-		return direccion;
+	public String getDireccion() {
+		return direccion.callePrincipal + " " + direccion.altura;
 	}
-
+	
 	public void setDireccion(Direccion direccion) {
 		this.direccion = direccion;
 	}
 
-	public GeoLocalizacion getGeoloc() {
+	public Location getGeoloc() {
 		return geoloc;
 	}
 
-	public void setGeoloc(GeoLocalizacion geoloc) {
+	public void setGeoloc(Location geoloc) {
 		this.geoloc = geoloc;
 	}
 
@@ -47,5 +49,44 @@ public class Poi {
 	public void setEstadistica(Estadistica estadistica) {
 		this.estadistica = estadistica;
 	}
-    
-}
+
+	public double calcularDistanciaA(Location geoloc) {
+		
+		double  latitud, longitud, miLatitud, miLongitud;
+		Location miLocalizacion;
+		
+		miLocalizacion = this.getGeoloc();
+		
+		miLatitud = miLocalizacion.getLatitude();
+		miLongitud = miLocalizacion.getLongitude();
+		
+		latitud = geoloc.getLatitude();
+		longitud = geoloc.getLongitude();
+		
+		//private  double distance(double lat1, double lon1, double lat2, double lon2, String unit) {
+			double theta = miLongitud - longitud;
+			double dist = Math.sin(deg2rad(miLatitud)) * Math.sin(deg2rad(latitud)) + Math.cos(deg2rad(miLatitud)) * Math.cos(deg2rad(latitud)) * Math.cos(deg2rad(theta));
+			dist = Math.acos(dist);
+			dist = rad2deg(dist);
+			dist = dist * 60 * 1.1515;
+			dist = dist * 1.609344;
+	
+			return (dist);
+		}
+
+		/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+		/*::	This function converts decimal degrees to radians						 :*/
+		/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+		private static double deg2rad(double deg) {
+			return (deg * Math.PI / 180.0);
+		}
+
+		/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+		/*::	This function converts radians to decimal degrees						 :*/
+		/*:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::*/
+		private static double rad2deg(double rad) {
+			return (rad * 180 / Math.PI);
+		}
+	
+}		
+		
