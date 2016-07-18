87fcfb13e6a89d751c8c9b72199b521527d83e09
Vicente Javier Villalba Caceres <jvillalba@fighthawk.com>
2016-07-18 20:22:48 -0300

    FRAMEWORK E-MAIL

---
+++ tpdds/src/frameworkEmails/Email.java
@@ -0,0 +1,37 @@
+package frameworkEmails;
+
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+
+public class Email {
+
+	/**
+	 * FRAMEWORK ENVIO DE MAILS
+	 */
+	public static void enviar(String emailEnvio, String password, String emailRecibo, String asunto, String mensaje) {
+		// PROPIEDADES CONEXION EMAIL
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "smtp.yandex.com"); // SMTP Host
+		props.put("mail.smtp.socketFactory.port", "465"); // SSL Port
+		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); // SSL
+		props.put("mail.smtp.auth", "true"); // Enabling SMTP Authentication
+		props.put("mail.smtp.port", "465"); // SMTP Port
+
+		// AUTENTIFICACION EMAIL
+		Authenticator auth = new Authenticator() {
+			// override the getPasswordAuthentication method
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(emailEnvio, password);
+			}
+		};
+
+		// SESION Y ENVIO EMAIL
+		Session session = Session.getDefaultInstance(props, auth);
+		FEmail.enviar(session, emailRecibo, emailEnvio, asunto, mensaje);
+
+	}
+
+}
---
+++ tpdds/src/frameworkEmails/FEmail.java
@@ -0,0 +1,45 @@
+package frameworkEmails;
+
+import java.util.Date;
+
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class FEmail {
+
+	/**
+	 * FRAMEWORK E-MAILS
+	 */
+	public static void enviar(Session session, String toEmail, String ccEmail, String subject, String body) {
+		try {
+			MimeMessage msg = new MimeMessage(session);
+			// set message headers
+			msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+			msg.addHeader("format", "flowed");
+			msg.addHeader("Content-Transfer-Encoding", "8bit");
+
+			msg.setFrom(new InternetAddress(ccEmail, "TERMINAL POIS"));
+
+			msg.setReplyTo(InternetAddress.parse(ccEmail, false));
+
+			msg.setSubject(subject, "UTF-8");
+
+			msg.setText(body, "UTF-8");
+
+			msg.setSentDate(new Date());
+
+			msg.addRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
+			msg.addRecipients(Message.RecipientType.CC, InternetAddress.parse(ccEmail, false));
+			Transport.send(msg);
+			
+			//CAMBIAR POR UN AVISO DE VENTANA
+			System.out.println("EMAIL ENVIADO EXITOSAMENTE!!");
+		} catch (Exception e) {
+			//CAMBIAR POR UN AVISO DE VENTANA
+			System.out.println("ERROR AL TRATAR DE ENVIAR MAIL");
+		}
+	}
+}
