c6ffe42447e717c3673dbf2b92bba08eacf213de
EzeCastro <ezec09@hotmail.com>
2016-09-07 11:46:37 -0300

    ingreso correcto de usuarios

--- tpdds/src/tpdds/Usuarios/Usuario.java
+++ tpdds/src/tpdds/Usuarios/Usuario.java
@@ -1,5 +1,12 @@
 package tpdds.Usuarios;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
+
+@MappedSuperclass
 public interface Usuario {
 
 	public String getNombreUsuario();
