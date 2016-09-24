23bed820abe333e139cf55a380a270167d0b38b5
EzeCastro <ezec09@hotmail.com>
2016-09-23 21:04:14 -0300

    empiezo con la creacion del tipo de usuario

---
+++ tpdds/src/tpdds/interfaz/AgregarTipoUsuario.java
@@ -0,0 +1,41 @@
+package tpdds.interfaz;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+
+public class AgregarTipoUsuario implements Initializable {
+
+	@FXML
+	CheckBox permInsPoi;
+	@FXML
+	CheckBox permVRF;
+	@FXML
+	CheckBox permCTU;
+	@FXML
+	CheckBox permMTU;
+	@FXML
+	CheckBox permVRTT;
+	@FXML
+	CheckBox permBusc;
+	@FXML
+	CheckBox permLP;
+	@FXML
+	CheckBox permVRTM;
+	@FXML
+	CheckBox permModPoi;
+	@FXML
+	CheckBox permModUser;
+	
+	
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
