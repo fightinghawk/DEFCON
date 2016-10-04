e840c2b6a5edc12736b34ebfcdda6af1e347d5c7
Daniel <Daniel@Daniel-PC>
2016-10-04 03:42:19 -0300

    Fix Lanzadorv4

--- tpdds/src/tpdds/interfaz/LanzadorDeProcesos.java
+++ tpdds/src/tpdds/interfaz/LanzadorDeProcesos.java
@@ -3,35 +3,23 @@ package tpdds.interfaz;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.control.TableView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.util.Callback;
-import tpdds.interfaz.componentes.ObsPoi;
 import tpdds.interfaz.componentes.ObsProceso;
-import tpdds.interfaz.componentes.ObsUser;
-import tpdds.pois.Poi;
 import tpdds.proceso.Proceso;
 
-public class LanzadorDeProcesos implements Initializable {
+public class LanzadorDeProcesos extends Escena{
 	
 	long time_start, time_end;
 	ArrayList<Proceso> listaProcesos;
@@ -55,28 +43,6 @@ public class LanzadorDeProcesos implements Initializable {
 	Stage nuevaStage;
 	FXMLLoader loader;
 	AnchorPane rootLayout;
-	
-	public void lanzadorRender(){
-		try{
-			//Carga archivo FXML  q tiene la interfaz
-			loader = new FXMLLoader();
-			loader.setLocation(Main.class.getResource("lanzadorDeProcesos.fxml"));
-			loader.setController(this);
-			rootLayout = loader.load();
-			//Creo Scene y la configuro
-			Scene scene = new Scene(rootLayout);
-			//Stage a abrirse
-			nuevaStage = new Stage();
-			nuevaStage.initModality(Modality.WINDOW_MODAL);
-			nuevaStage.initOwner(Main.primaryStage);
-			nuevaStage.setResizable(false);
-			nuevaStage.setTitle("Lanzador de Procesos");
-			nuevaStage.setScene(scene);
-			nuevaStage.show();
-		}catch(Exception ex){
-			ex.printStackTrace();
-		}
-	}
 	
 	@FXML
 	
